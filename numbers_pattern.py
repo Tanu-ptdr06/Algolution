9363c69bb2f9fa0f31e962d256e6b1a1828a4946
Tanu-ptdr06 <tanupatidar855@gmail.com>
2024-12-09 07:07:50 -0800

    Update numbers_pattern.py

--- numbers_pattern.py
+++ numbers_pattern.py
@@ -1,9 +1,12 @@
 n=5
-for i in range(1):
-    for j in range(n):
-        for k in range(j+1,n+1):
+for i in range(n):
+    for j in range(1):
+       if i%2==0:
+        for m in range(n):
+          print(m+1,end='')
+       else:
+          for k in range(n,0,-1):
             print(k,end='')
-        for l in range(j+1):
-            print('5',end='')
-        print()
+    for l in range(n):
+        print('',end='')
     print()
